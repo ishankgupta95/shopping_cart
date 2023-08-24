bc86be588861f78c42680d19b745978e1b50eca9
Ishank Gupta <ishank1995@gmail.com>
2023-08-24 13:45:07 +0530

    changed name of NavBar component to NavBarComponent

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 
-export default function NavBar() {
+export default function NavBarComponent() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,9 +1,9 @@
 import { Outlet } from "react-router-dom"
-import NavBar from "./NavBar"
+import NavBarComponent from "./NavBarComponent"
 export default function RootLayout() {
   return (
     <>
-      <NavBar />
+      <NavBarComponent />
       <main>
         <Outlet/>
       </main>
